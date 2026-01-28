f84eb4530460545ece5925f3a9b84c3cd1faf26f
Mrksist <platon_fil08@mail.ru>
2026-01-28 20:32:21 +0300

    A big update

--- js/main.js
+++ js/main.js
@@ -1,168 +1,136 @@
-const organizations = [
-    {
-        name: "МБОУ Лицей №3",
-        logo: "l3.jpg",
-        coordinates: [43.345155, 54.920030],
-        label_id: "lyceum3",
-        description: "Физико-математический лицей",
-        info: [
-            "Количество учащихся: 2026 человек",
-            "Количество мест: НЕмало",
-            "Количество классов: -22",
-            `Ближайший детский сад: <a class="a-near" onclick="showPath('sch3ds6')">Сад №6</a>`
-        ]
-    },
-    {
-        name: "МБОУ Гимназия №2",
-        logo: "g2.png",
-        coordinates: [43.352058, 54.925033],
-        label_id: "gymnasium2",
-        description: "Гимназия с языковым уклоном",
-        info: [
-            "Количество учащихся: 2025 человек",
-            "Количество мест: НЕмало",
-            "Количество классов: гойда",
-            `Ближайший детский сад: <a class="a-near" onclick="showPath('sch2ds36')">Сад №36</a>`
-        ]
-    },
-    {
-        name: "Детский сад №6",
-        coordinates: [43.344887, 54.919234],
-        label_id: "detsad6",
-        description: "Сад с дошкольным образованием",
-        info: [
-            "Количество мест: НЕмало",
-            "Количество групп: ещё больше",
-            `Ближайшая школа: <a class="a-near" onclick="showPath('sch3ds6')">МБОУ Лицей №3</a>`
-        ]
-    },
-    {
-        name: "Детский сад №9",
-        coordinates: [43.348281, 54.920800],
-        description: "",
-        label_id: "detsad9",
-        info: [
-            "Количество мест: скажем",
-            "Количество групп: скажем",
-            `Ближайшая школа: <a class="a-near" onclick="showPath('sch3ds9')">МБОУ Лицей №3</a>`
-        ]
-    }
-]
-
-const paths = {
-    sch3ds6: {
-        coords: [
-            [43.345285, 54.919847],
-            [43.345001, 54.919371]  
-        ],
-        centercoords: [43.345173, 54.919637],
-        dist: "60м"
-    },
-    sch3ds9: {
-        coords: [
-            [43.345246, 54.919800],
-            [43.348087, 54.919242],
-            [43.348985, 54.920705],
-            [43.348628, 54.920774]
-        ],
-        centercoords: [43.348712, 54.920270],
-        dist: "400м"
-    }
-}
-
-let routes = [];
+let paths = [];
 let map = null;
 
 function newMarker(organization) {
-    const markerElement = document.createElement('div')
-    let innerHTML = ``;
+    let markerElement;
+    for (let i in organization.coordinates) {
+        markerElement = document.createElement('div')
+        let innerHTML = ``;
 
-    innerHTML += `<div class="school-marker">
-                        <img src="img/${organization.logo ?? 'default.png'}" class="school-marker-img"></img>
+        innerHTML += `<div class="school-marker">
+                        <img src="img/${organization.id + `z${parseInt(i) + 1}.png`}" class="school-marker-img"></img>
                     </div>`
-    innerHTML += `<h3 class="school-marker-label">${organization.name}</h3>`
+        innerHTML += `<h3 class="school-marker-label">${organization.name}</h3>`
 
-    markerElement.innerHTML = innerHTML
-    markerElement.className = "school-marker-wrapper"
-    markerElement.onclick = function () {
-        document.getElementById(organization.label_id).style.display = 'flex';
+        markerElement.innerHTML = innerHTML
+        markerElement.className = "school-marker-wrapper"
+        markerElement.onclick = function () {
+            document.getElementById(organization.id).style.display = 'flex';
+        }
+
+        map.addChild(new ymaps3.YMapMarker(
+            {
+                coordinates: organization.coordinates[i],
+                id: organization.id + i
+            },
+            markerElement
+        ));
     }
-
-    return new ymaps3.YMapMarker(
-        {
-            coordinates: organization.coordinates,
-            id: organization.label_id
-        },
-        markerElement
-    );
 }
 
-function newModal(organization){
-    let body = document.getElementById("main");
+function newModal(organization) {
+    let root = document.getElementById("main");
     let innerHTML = ``
 
     innerHTML += `
-         <div class="modal" id="${organization.label_id}" onclick="if(event.target == this) this.style.display = 'none'">
+         <div class="modal" id="${organization.id}" onclick="if(event.target == this) this.style.display = 'none'">
             <div class="modal-content">
+                <img src="img/${organization.id + 'z1.png'}" class="modal-img"></img>
               <h2>${organization.name}</h2>
               <h4 style="text-align: center;">${organization.description}</h4>`;
-    for(let i of organization.info){
+    for (let i of organization.info) {
         innerHTML += `<p style="text-align: left">• ${i}</p>`;
     }
+
+    if (organization.nearby.length) {
+        innerHTML += `<p style="text-align: left">• ОО рядом: `
+    }
+
+    for (let i of organization.nearby) {
+        const nearOrg = organizations.find(a => a.id == i)
+        innerHTML += `<a class="a-near" onclick="showPath('${nearOrg.id}-${organization.id}')">${nearOrg.name}</a>, `
+    }
+    innerHTML = innerHTML.substring(0, innerHTML.length - 2);
     innerHTML += `</div></div>`
-    
-    body.innerHTML += innerHTML;
+
+    root.innerHTML += innerHTML;
 }
 
-function hidePaths() {
-    document.getElementById('hide-paths').style.display = 'none';
-    for(let i of routes){
-        map.removeChild(i);
-    }
-    routes = [];
+
+function calculateDistance(lon1, lat1, lon2, lat2) {
+  const R = 6371e3;
+  const phi1 = lat1 * Math.PI / 180;
+  const phi2 = lat2 * Math.PI / 180;
+  const deltaphi = (lat2 - lat1) * Math.PI / 180;
+  const deltalambda = (lon2 - lon1) * Math.PI / 180;
+
+  const a = Math.sin(deltaphi / 2) * Math.sin(deltaphi / 2) +
+            Math.cos(phi1) * Math.cos(phi2) *
+            Math.sin(deltalambda / 2) * Math.sin(deltalambda / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c;
 }
 
-function hideModals() {
-    for(let i of document.getElementsByClassName('modal')){
-        i.style.display = 'none';
-    }
-}
-
-function showPath(path){
+async function showPath(path) {
     hideModals();
     hidePaths();
-    
+
     document.getElementById('hide-paths').style.display = 'flex';
 
-    const pathObject = paths[path]
+    const dos = path.split('-');
+
+    const firstDO = organizations.find(a => a.id === dos[0]);
+    const secondDO = organizations.find(a => a.id === dos[1]);
 
     const lineStringFeature = new ymaps3.YMapFeature({
         id: 'line',
         geometry: {
             type: 'LineString',
-            coordinates: pathObject.coords
+            coordinates: [
+                firstDO.coordinates[0],
+                secondDO.coordinates[0]
+            ]
         },
         style: {
-            stroke: [{ width: 12, color: 'rgb(246, 52, 52)' }]
+            stroke: [{ width: 12, color: 'rgb(242, 167, 46)' }]
         }
     });
 
+    const centercoords = [(firstDO.coordinates[0][0] + secondDO.coordinates[0][0]) / 2, (firstDO.coordinates[0][1] + secondDO.coordinates[0][1]) / 2];
+
     const markerElement = document.createElement('div');
-    markerElement.style.width = "40px";
+    markerElement.style.width = "70px";
     markerElement.style.fontSize = "30px";
-    markerElement.innerText = pathObject.dist;
+
+    const len = calculateDistance(firstDO.coordinates[0][0], firstDO.coordinates[0][1], secondDO.coordinates[0][0],  secondDO.coordinates[0][1])
+    markerElement.innerText = Math.round(len) + 'м';
 
     const marker = new ymaps3.YMapMarker(
         {
-            coordinates: pathObject.centercoords
+            coordinates: centercoords
         },
         markerElement
     );
 
-    routes.push(lineStringFeature);
-    routes.push(marker);
+    paths.push(marker);
+    paths.push(lineStringFeature);
     map.addChild(marker);
     map.addChild(lineStringFeature);
+}
+
+function hidePaths() {
+    document.getElementById('hide-paths').style.display = 'none';
+    for (let i of paths) {
+        map.removeChild(i);
+    }
+    paths = [];
+}
+
+function hideModals() {
+    for (let i of document.getElementsByClassName('modal')) {
+        i.style.display = 'none';
+    }
 }
 
 function initMaps() {
@@ -176,18 +144,18 @@ function initMaps() {
         }
     );
 
-    map.addChild(new ymaps3.YMapDefaultSchemeLayer({ 
+    map.addChild(new ymaps3.YMapDefaultSchemeLayer({
         customization: [{
-                tags: 'poi',
-                stylers: {
-                    visibility: "off"
-                }
-            }]
-        }));
+            tags: 'poi',
+            stylers: {
+                visibility: "off"
+            }
+        }]
+    }));
 
     map.addChild(new ymaps3.YMapDefaultFeaturesLayer());
     for (let i of organizations) {
-        map.addChild(newMarker(i));
+        newMarker(i);
         newModal(i);
     }
 }
